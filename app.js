77de84485a6c7ebc38aec889566ece964404481d
prajyot <prajyot@sapnagroup.com>
2021-06-17 17:40:06 +0530

    db fetch added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const cafeList = document.querySelector('#cafe-list')
+
+function render_element(doc){
+    console.log(doc.data().name+" => "+doc.data().city);
+}
+
 db.collection('cafes').get().then((snapshot)=>{
     console.log('--db loaded--');
     console.log(snapshot.docs);
     snapshot.docs.forEach(doc=>{
-        console.log(doc.data());
+        render_element(doc)
     })
 })
