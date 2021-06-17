a6ef97aecf2f60ab687666ed99b95f120b320bb5
prajyot <prajyot@sapnagroup.com>
2021-06-17 22:22:22 +0530

    adding data to the list

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const cafeList = document.querySelector('#cafe-list');
+const cafeList      = document.querySelector('#cafe-list');
+const formObj       = document.getElementById('add-cafe-form');
+const hotel_name    = document.getElementById('hotel_name');
+const hotel_city    = document.getElementById('hotel_city');
 
 function render_element(doc){
 
@@ -15,3 +18,16 @@ db.collection('cafes').get().then((snapshot)=>{
         render_element(doc)
     })
 })
+
+function submit_form(){
+    if(hotel_name.value && hotel_city.value){
+        console.log('111111111111');
+
+        db.collection('cafes').add({
+            name:hotel_name.value,
+            city:hotel_city.value
+        });
+
+        hotel_name.value  ,hotel_city.value = '';
+    }
+}
